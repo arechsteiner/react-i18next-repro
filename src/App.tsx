9ad3d4a5698cdcf6725ccd34ac483ac234efb545
Alexander Rechsteiner <alex@wildmind.ch>
2024-03-06 12:43:31 +0100

    Reproduce issue and prettify code

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
+import i18n from 'i18next'
+import { useTranslation, initReactI18next } from 'react-i18next'
+import resourcesToBackend from 'i18next-resources-to-backend'
+import en from './locales/en/translation.json'
 
 i18n
+  .use(
+    resourcesToBackend((language, namespace, callback) => {
+      console.log('namespace', namespace, language)
+      callback(null, en)
+    }),
+  )
   .use(initReactI18next)
   .init({
-    resources: {
-      en: {
-        translation: {
-          "my_key": "Welcome to React and react-i18next"
-        }
-      }
+    defaultNS: 'common',
+    lng: 'en',
+    fallbackLng: 'en',
+    react: {
+      useSuspense: false,
     },
-    lng: "en",
-    fallbackLng: "en",
-
     interpolation: {
-      escapeValue: false
-    }
-  });
+      escapeValue: false,
+    },
+  })
 
 function App() {
-  const [count, setCount] = useState(0)
   const { t } = useTranslation()
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>{t('my_key')}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
